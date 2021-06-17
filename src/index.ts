38c20205fce1a7e2b5eb9b2b6606cde93613a3b9
reslear <reslear@gmail.com>
2021-06-17 03:29:14 +0300

    refactor: define types, router 4 support

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,61 @@
-import { Route, NavigationGuardNext } from 'vue-router'
+import { store } from '@/store'
+import {
+  NavigationGuard,
+  NavigationGuardNext,
+  RouteLocationNormalized,
+} from 'vue-router'
 
-export default (datas?: any) => async (to: Route, from: Route, next: NavigationGuardNext) => {
-
-  if (to.meta?.middleware?.length) {
-    const arr = to.meta.middleware
-    for (let index = 0; index < arr.length; index++) {
-      const method: Function = arr[index];
-      const result = method({...datas, to, from, next})
-      if (result === false) {
-        break
-      }
-    }
-    return
-  }
-
-  return next()
+// TODO: refactor 
+declare interface RouterMiddlewareContext {
+  store: typeof store
 }
+
+type RouterGuardCb = {
+  to: RouteLocationNormalized
+  from: RouteLocationNormalized
+  next: NavigationGuardNext
+}
+
+type DefineRouterMiddleware = (
+  ctx: Partial<RouterMiddlewareContext> & RouterGuardCb
+) => ReturnType<NavigationGuard> | false
+
+
+/** 
+ * function helper for define types
+ * @example
+ * defineRouterMiddleware(({ from, to, store }) => {
+ *  // code
+ * })
+**/
+export const defineRouterMiddleware = (
+  m: DefineRouterMiddleware
+): DefineRouterMiddleware => m
+
+declare module 'vue-router' {
+  interface RouteMeta {
+    middleware?: DefineRouterMiddleware[]
+  }
+}
+
+export const routerMiddleware =
+  (ctx: Partial<RouterMiddlewareContext> = {}) =>
+  async (
+    to: RouteLocationNormalized,
+    from: RouteLocationNormalized,
+    next: NavigationGuardNext
+  ) => {
+    if (to.meta?.middleware?.length) {
+      const arr = to.meta.middleware
+      for (let index = 0; index < arr.length; index++) {
+        const method = arr[index]
+        const result = method({ ...ctx, to, from, next })
+        if (result === false) {
+          break
+        }
+      }
+      return
+    }
+
+    return next()
+  }
